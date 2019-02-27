de825898133443ea036c4e31dcccd0d2c9169bb8
Salma Virk <salma@scalefactory.com>
2019-02-27 16:50:07 +0000

    More Gin

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Gin world\n');
+  res.send('Hello TEA Gin world\n');
 });
 
 app.listen(PORT, HOST);
